1499fd6001c32f3acba86c704ab1bb0b933e94d5
konkeror <sid.mkjee@gmail.com>
2022-10-05 06:59:36 +0200

    Initialise testing.

---
+++ code/Testing.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------------
+# --- TESTING ---
+# ---------------------------------------------------------------------------------
+
+def get_classification(features_summary , input_instances):
+    classifications = []
+    for i, input in list(enumerate(input_instances)):
+        prediction = classify(features_summary, input)
+        classifications.append(prediction)
+    return classifications
+
+
+def accuracy(test_labels , predicted_labels):
+    true_positive = 0
+    for i in range(len(test_labels)):
+        if test_labels[i] == predicted_labels[i]:
+            true_positive += 1
+    score = true_positive / float(len(test_labels))
+    return (score*100)
