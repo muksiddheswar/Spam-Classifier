b544329febf34011ed9acdf56cd8deb5ab3ea57e
konkeror <sid.mkjee@gmail.com>
2022-10-08 01:39:44 +0200

    Created main.

---
+++ main.py
@@ -0,0 +1,27 @@
+from lib.Naive_Bayes_Classifier import naive_bayes_classifier
+import os
+
+
+def main():
+    if os.path.isfile("config.txt"):
+
+        f = open("config.txt", "r")
+        line = f.readline().rstrip('\n')
+        f.close()
+
+        datafolder = os.path.normcase(line.split("=")[1])
+
+        train_dir = os.path.join(datafolder, "train-mails")
+        test_dir = os.path.join(datafolder, "test-mails")
+
+
+    else:
+        train_dir = os.path.join(os.path.curdir, "train-mails")
+        test_dir = os.path.join(os.path.curdir, "test-mails");
+
+    naive_bayes_classifier(train_dir, test_dir)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
