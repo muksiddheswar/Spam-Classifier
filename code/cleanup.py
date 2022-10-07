f64ea8570afb65ae40e0514142cc3915dcb70b7b
konkeror <sid.mkjee@gmail.com>
2022-10-08 00:20:45 +0200

    Added cleanup script.

---
+++ code/cleanup.py
@@ -0,0 +1,9 @@
+import os
+
+file = "class_count.txt"
+os.remove(file)
+print("%s has been removed successfully" % file)
+
+file = "mail_count.txt"
+os.remove(file)
+print("%s has been removed successfully" % file)
